d9c4702b2f0d42086ea3ad9e8eb1ebbc0e395f45
Nikola Milosevic <240391250+nikola92-sudone@users.noreply.github.com>
2026-05-27 13:00:10 +1000

    Templates added

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure_h77%b+!j5zme=4wqusgsib@-6z+j_=_izr-jxb2vrm&!al7l-'
+SECRET_KEY = '_h77%b+!j5zme=4wqusgsib@-6z+j_=_izr-jxb2vrm&!al7l-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
